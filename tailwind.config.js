4625833f1d22f99a9c6d7a760335fc0b9c663002
Nils <52067471+nils-n@users.noreply.github.com>
2024-01-09 12:19:37 +0000

    add config for flowbite css components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.{html,js}", "./templates/*.{html,js}"],
+  content: [
+    "./templates/**/*.{html,js}",
+    "./templates/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
